b957f7f37c82455d5ce15624f9bd95402be099d9
Serhii Kornelyuk <seiger@meta.ua>
2023-06-13 22:41:40 +0300

    [ADD] Socials tab.

--- config/sSettingsSettings.php
+++ config/sSettingsSettings.php
@@ -3,28 +3,39 @@
         "label" => "sSettings::global.basicTab",
         "fields" => [
             [
-                "name" => "email",
-                "label" => "sSettings::global.email",
-                "description" => "sSettings::global.email_description",
-                "type" => "text",
-            ],
-            [
                 "name" => "phone",
                 "label" => "sSettings::global.phone",
                 "description" => "sSettings::global.phone_description",
                 "type" => "text",
             ],
+        ]
+    ],
+    "socialsTab" => [
+        "label" => "sSettings::global.socialsTab",
+        "fields" => [
             [
-                "name" => "phones",
-                "label" => "sSettings::global.phones",
-                "description" => "sSettings::global.phones_description",
+                "name" => "facebook",
+                "label" => "sSettings::global.facebook",
+                "description" => "sSettings::global.facebook_description",
                 "type" => "text",
             ],
             [
-                "name" => "google_analytics",
-                "label" => "sSettings::global.google_analytics",
-                "description" => "sSettings::global.google_analytics_description",
-                "type" => "textarea",
+                "name" => "instagram",
+                "label" => "sSettings::global.instagram",
+                "description" => "sSettings::global.instagram_description",
+                "type" => "text",
+            ],
+            [
+                "name" => "linkedin",
+                "label" => "sSettings::global.linkedin",
+                "description" => "sSettings::global.linkedin_description",
+                "type" => "text",
+            ],
+            [
+                "name" => "twitter",
+                "label" => "sSettings::global.twitter",
+                "description" => "sSettings::global.twitter_description",
+                "type" => "text",
             ],
         ]
     ],
